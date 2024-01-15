3bc717579c3f6991c379d975d5c5ac1961201a60
Leon <94672224+Leon4ever2802@users.noreply.github.com>
2024-01-15 15:48:33 +0100

    clean up

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
+# -----------------------------------------------------------------------------
+# Author               :   Leon Reusch
+# -----------------------------------------------------------------------------
+
+# this is the only thing you have to edit when using the whole code itself
+
 SETTINGS: dict = {
     # WIFI:
     "SSID": "[Enter SSID here]",
